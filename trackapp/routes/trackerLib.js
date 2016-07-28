c4d635ad564b67f7baafb30235781c4d7c2478e4
jr <rodriorquiola@gmail.com>
2016-07-27 23:06:48 -0400

    Tarea I de Rodrigo completada. 2 nuevos recursos.

--- trackapp/routes/trackerLib.js
+++ trackapp/routes/trackerLib.js
@@ -20,8 +20,8 @@ var formData={
    "Content-Type":"application/x-www-urlencoded",
   },
   method:"POST",
-  //host:"52.37.150.11:8082",
-  uri:"http://52.37.150.11:8082/api/session",
+  //host:"52.42.220.194:5082",
+  uri:"http://52.42.222.59:8082/api/session",
   form:formData
 },function(error, response, body)
 {
@@ -35,6 +35,8 @@ allSes=cookies;
 
  
 });
+
+
 router.get("/positions",function(req,res)
 {
 
@@ -45,11 +47,50 @@ console.log(ses);
     "Cookie":allSes != null?allSes[0]:null
  
   },
- uri:"http://52.37.150.11:8082/api/positions"
+ uri:"http://52.42.255.179:8082/api/positions"
+
+},function(error,response,body){
+ res.statusCode=response.statusCode.toString(); 
+ res.json(response);
+}
+);
+});
+
+//RODRIGO
+
+router.get("/devices",function(req,res)
+{
+//Cookie:JSESSIONID=ffyneiefc5fzmz05gw71o5hs
+console.log(ses);
+ request({
+  "headers":{
+    "Cookie":allSes != null?allSes[0]:null
+ 
+  },
+ uri:"http://52.42.255.179:8082/api/devices?_dc=1469673668161&page=1&start=0&limit=25"
  
 },function(error,response,body)
 {
-  
+ res.statusCode=response.statusCode.toString(); 
+ res.json(response);
+}
+);
+});
+
+router.get("/serverSettings",function(req,res)
+{
+//Cookie:JSESSIONID=ffyneiefc5fzmz05gw71o5hs
+console.log(ses);
+ request({
+  "headers":{
+    "Cookie":allSes != null?allSes[0]:null
+ 
+  },
+ uri:"http://52.42.255.179:8082/api/server?_dc=1469673651304"
+ 
+},function(error,response,body)
+{
+ res.statusCode=response.statusCode.toString(); 
  res.json(response);
 }
 );
